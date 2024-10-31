c421bf3c6f3ca3085e94f567450f3f08f845f74c
medetisrafilov <israfilovmedet52@gmail.com>
2024-10-30 18:22:51 -1200

    homework

---
+++ abb.js
@@ -0,0 +1,17 @@
+let fulname ="Medet Israfiov"
+console.log(fullName[0] , fullName [5]);
+
+
+const fullName = "Medet israfilov"
+console.log(FullName.replace ("Medet israfilov", "Israfilov Medet"));
+
+
+let text ="Medet Israfilov"
+console.log(text.length - 1);
+
+
+let firstName = "Medet"
+let lastName ="Seyidov"
+console.log ( "Sizin Adiniz" + " " + firstName + " " + lastName + "." + "Sizi bir daha gormeyize Sadiq,")
+
+
